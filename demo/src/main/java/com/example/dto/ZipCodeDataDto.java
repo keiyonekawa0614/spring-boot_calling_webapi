497711a45bbf9b2ac043e0f0e2370b4381880cf2
keiyonekawa0614 <kei@yonekawakatsura-no-MacBook-Pro.local>
2016-11-19 17:35:33 +0900

    20161120

--- demo/src/main/java/com/example/dto/ZipCodeDataDto.java
+++ demo/src/main/java/com/example/dto/ZipCodeDataDto.java
@@ -3,28 +3,28 @@ package com.example.dto;
 public class ZipCodeDataDto {
 	
 	/** 郵便番号 */
-	String zipcode = null;
+	String zipcode;
 	
 	/** 都道府県コード */
-	String prefcode = null;
+	String prefcode;
 	
 	/** 都道府県名 */
-	String address1 = null;
+	String address1;
 	
 	/** 市区町村名 */
-	String address2 = null;
+	String address2;
 	
 	/** 町域名 */
-	String address3 = null;
+	String address3;
 	
 	/** 都道府県名カナ */
-	String kana1 = null;
+	String kana1;
 	
 	/** 市区町村名カナ */
-	String kana2 = null;
+	String kana2;
 	
 	/** 町域名カナ */
-	String kana3 = null;
+	String kana3;
 
 	public String getZipcode() {
 		return zipcode;
